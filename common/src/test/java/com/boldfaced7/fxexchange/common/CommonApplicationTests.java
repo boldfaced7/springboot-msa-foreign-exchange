97278689979f41c749f8456b4c4b1546b553e391
boldfaced7 <boldfaced7@naver.com>
2025-05-22 20:52:12 +0900

    Common 모듈 개발 - 헥사고날 아키텍처용 어노테이션 작성

---
+++ common/src/test/java/com/boldfaced7/fxexchange/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boldfaced7.fxexchange.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
