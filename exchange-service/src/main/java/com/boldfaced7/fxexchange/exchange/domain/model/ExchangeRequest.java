cc18252907171486ad6d707b09c4ea0b73e7ae8e
boldfaced7 <boldfaced7@naver.com>
2025-06-04 17:08:04 +0900

    도메인 클래스에 통합/추가된 이벤트와 VO 반영

--- exchange-service/src/main/java/com/boldfaced7/fxexchange/exchange/domain/model/ExchangeRequest.java
+++ exchange-service/src/main/java/com/boldfaced7/fxexchange/exchange/domain/model/ExchangeRequest.java
@@ -2,21 +2,26 @@ package com.boldfaced7.fxexchange.exchange.domain.model;
 
 import com.boldfaced7.fxexchange.exchange.domain.enums.Direction;
 import com.boldfaced7.fxexchange.exchange.domain.event.DomainEvent;
-import com.boldfaced7.fxexchange.exchange.domain.event.buy.*;
-import com.boldfaced7.fxexchange.exchange.domain.event.sell.*;
+import com.boldfaced7.fxexchange.exchange.domain.event.deposit.*;
+import com.boldfaced7.fxexchange.exchange.domain.event.request.ExchangeCurrencyFailed;
+import com.boldfaced7.fxexchange.exchange.domain.event.request.ExchangeCurrencyStarted;
+import com.boldfaced7.fxexchange.exchange.domain.event.request.ExchangeCurrencySucceeded;
+import com.boldfaced7.fxexchange.exchange.domain.event.request.SendingWarningMessageRequired;
+import com.boldfaced7.fxexchange.exchange.domain.event.withdrawal.*;
 import com.boldfaced7.fxexchange.exchange.domain.vo.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@ToString
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExchangeRequest {
-
     private RequestId requestId;
     private ExchangeId exchangeId;  // 외부 추적 가능한 고유 ID
     private UserId userId;
@@ -31,6 +36,7 @@ public class ExchangeRequest {
     
     private WithdrawalId withdrawalId;      // 출금 트랜잭션 ID
     private DepositId depositId;       // 입금 트랜잭션 ID
+    private WithdrawalCancelId withdrawalCancelId; // 출금 취소 트랜잭션 ID
     
     private Finished finished;
     private LocalDateTime createdAt;
@@ -60,6 +66,7 @@ public class ExchangeRequest {
                 exchangeRate,
                 null,
                 null,
+                null,
                 new Finished(false),
                 LocalDateTime.now(),
                 LocalDateTime.now()
@@ -78,6 +85,7 @@ public class ExchangeRequest {
         ExchangeRate exchangeRate,
         WithdrawalId withdrawalId,
         DepositId depositId,
+        WithdrawalCancelId withdrawalCancelId,
         Finished finished,
         LocalDateTime createdAt,
         LocalDateTime updatedAt
@@ -94,6 +102,7 @@ public class ExchangeRequest {
                 exchangeRate,
                 withdrawalId,
                 depositId,
+                withdrawalCancelId,
                 finished,
                 createdAt,
                 updatedAt
@@ -110,12 +119,16 @@ public class ExchangeRequest {
         return finished.value();
     }
 
+    public void addWithdrawalId(WithdrawalId withdrawalId) {
+        this.withdrawalId = withdrawalId;
+    }
+
     public void addDepositId(DepositId depositId) {
         this.depositId = depositId;
     }
 
-    public void addWithdrawalId(WithdrawalId withdrawalId) {
-        this.withdrawalId = withdrawalId;
+    public void addWithdrawalCancelId(WithdrawalCancelId withdrawalCancelId) {
+        this.withdrawalCancelId = withdrawalCancelId;
     }
 
     public void terminate() {
@@ -123,92 +136,82 @@ public class ExchangeRequest {
     }
 
     /*
-        환전(외화 구매) 관련 이벤트 생성
+        환전 관련 이벤트 생성
      */
-    public void buyingStarted() {
-        events.add(new BuyingStarted(requestId));
+    public void exchangeCurrencyStarted() {
+        events.add(new ExchangeCurrencyStarted(requestId, exchangeId, direction));
     }
 
-    public void buyingFailed() {
-        events.add(new BuyingFailed(requestId));
+    public void exchangeCurrencySucceeded() {
+        events.add(new ExchangeCurrencySucceeded(requestId, exchangeId, direction));
     }
 
-    public void buyingCompleted() {
-        events.add(new BuyingCompleted(requestId));
+    public void exchangeCurrencyFailed() {
+        events.add(new ExchangeCurrencyFailed(requestId, exchangeId, direction));
     }
 
     /*
-        원화 출금 관련 이벤트 생성
+        출금 관련 이벤트 생성
      */
-    public void checkingKrwWithdrawalRequired(Count count) {
-        events.add(new CheckingKrwWithdrawalRequired(requestId, count));
+    public void withdrawalSucceeded() {
+        events.add(new WithdrawalSucceeded(requestId, exchangeId, direction));
     }
 
-    public void delayingKrwWithdrawalCheckRequired(Count count) {
-        events.add(new DelayingKrwWithdrawalCheckRequired(requestId, count));
+    public void withdrawalFailed() {
+        events.add(new WithdrawalFailed(requestId, exchangeId, direction));
     }
 
-    public void cancelingKrwWithdrawalRequired() {
-        events.add(new CancelingKrwWithdrawalRequired(requestId, exchangeId));
+    public void withdrawalResultUnknown(Count count) {
+        events.add(new WithdrawalResultUnknown(requestId, exchangeId, direction, count));
     }
 
-    public void krwWithdrawalCompleted() {
-        events.add(new KrwWithdrawalCompleted(requestId));
-    }
-    
-    /*
-        외화 입금 관련 이벤트 생성
-     */
-    public void checkingFxDepositRequired(Count count) {
-        events.add(new CheckingFxDepositRequired(requestId, count));
+    public void withdrawalSuccessChecked() {
+        events.add(new WithdrawalSuccessChecked(requestId, exchangeId, direction));
     }
 
-    public void delayingFxDepositCheckRequired(Count count) {
-        events.add(new DelayingFxDepositCheckRequired(requestId, count));
+    public void withdrawalFailureChecked() {
+        events.add(new WithdrawalFailureChecked(requestId, exchangeId, direction));
     }
 
-    /*
-        환전(외화 판매) 관련 이벤트 생성
-     */
-    public void sellingStarted() {
-        events.add(new SellingStarted(requestId));
+    public void withdrawalCancelled() {
+        events.add(new WithdrawalCancelled(requestId, exchangeId, direction));
     }
 
-    public void sellingFailed() {
-        events.add(new SellingFailed(requestId));
-    }
-
-    public void sellingCompleted() {
-        events.add(new SellingCompleted(requestId));
+    public void delayingWithdrawalCheckRequired(Count count) {
+        events.add(new DelayingWithdrawalCheckRequired(requestId, exchangeId, direction, count));
     }
 
     /*
-        외화 출금 관련 이벤트 생성
+        입금 관련 이벤트 생성
      */
-    public void checkingFxWithdrawalRequired(Count count) {
-        events.add(new CheckingFxWithdrawalRequired(requestId, count));
+    public void depositSucceeded() {
+        events.add(new DepositSucceeded(requestId, exchangeId, direction));
     }
 
-    public void delayingFxWithdrawalCheckRequired(Count count) {
-        events.add(new DelayingFxWithdrawalCheckRequired(requestId, count));
+    public void depositFailed() {
+        events.add(new DepositFailed(requestId, exchangeId, direction));
     }
 
-    public void cancelingFxWithdrawalRequired() {
-        events.add(new CancelingFxWithdrawalRequired(requestId, exchangeId));
+    public void depositResultUnknown(Count count) {
+        events.add(new DepositResultUnknown(requestId, exchangeId, direction, count));
     }
 
-    public void fxWithdrawalCompleted() {
-        events.add(new FxWithdrawalCompleted(requestId));
+    public void depositSuccessChecked() {
+        events.add(new DepositSuccessChecked(requestId, exchangeId, direction));
+    }
+
+    public void depositFailureChecked() {
+        events.add(new DepositFailureChecked(requestId, exchangeId, direction));
+    }
+
+    public void delayingDepositCheckRequired(Count count) {
+        events.add(new DelayingDepositCheckRequired(requestId, exchangeId, direction, count));
     }
 
     /*
-        원화 입금 관련 이벤트 생성
+        경고 메시지 발송 관련 이벤트 생성
      */
-    public void checkingKrwDepositRequired(Count count) {
-        events.add(new CheckingKrwDepositRequired(requestId, count));
-    }
-
-    public void delayingKrwDepositCheckRequired(Count count) {
-        events.add(new DelayingKrwDepositCheckRequired(requestId, count));
+    public void sendingWarningMessageRequired(Count count) {
+        events.add(new SendingWarningMessageRequired(requestId, exchangeId, direction, count));
     }
 }
