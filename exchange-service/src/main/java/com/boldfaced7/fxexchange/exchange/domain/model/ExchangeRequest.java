13503438d582794d8fce36bb8b08faf7d9558ec5
boldfaced7 <boldfaced7@naver.com>
2025-06-19 16:46:27 +0900

    도메인 이벤트 클래스 이름 수정: 이벤트 클래스 이름에 비즈니스 로직이 남지 않도록 수정

--- exchange-service/src/main/java/com/boldfaced7/fxexchange/exchange/domain/model/ExchangeRequest.java
+++ exchange-service/src/main/java/com/boldfaced7/fxexchange/exchange/domain/model/ExchangeRequest.java
@@ -6,7 +6,6 @@ import com.boldfaced7.fxexchange.exchange.domain.event.deposit.*;
 import com.boldfaced7.fxexchange.exchange.domain.event.request.ExchangeCurrencyFailed;
 import com.boldfaced7.fxexchange.exchange.domain.event.request.ExchangeCurrencyStarted;
 import com.boldfaced7.fxexchange.exchange.domain.event.request.ExchangeCurrencySucceeded;
-import com.boldfaced7.fxexchange.exchange.domain.event.request.SendingWarningMessageRequired;
 import com.boldfaced7.fxexchange.exchange.domain.event.withdrawal.*;
 import com.boldfaced7.fxexchange.exchange.domain.vo.*;
 import lombok.AccessLevel;
@@ -23,19 +22,19 @@ import java.util.List;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExchangeRequest {
     private RequestId requestId;
-    private ExchangeId exchangeId;  // 외부 추적 가능한 고유 ID
+    private ExchangeId exchangeId;                 // 외부 추적 가능한 고유 ID
     private UserId userId;
     
-    private Direction direction;  // BUY: 원화 → 외화, SELL: 외화 → 원화
-    private BaseCurrency baseCurrency;  // 요청 금액 기준 통화
-    private QuoteCurrency quoteCurrency; // 상대 통화
+    private Direction direction;                   // BUY: 원화 → 외화, SELL: 외화 → 원화
+    private BaseCurrency baseCurrency;             // 외화
+    private QuoteCurrency quoteCurrency;           // 원화
     
-    private BaseAmount baseAmount;    // 기준 통화 금액
-    private QuoteAmount quoteAmount;   // 환전 후 통화 금액
-    private ExchangeRate exchangeRate;  // 적용된 환율
+    private BaseAmount baseAmount;                 // 외화 금액
+    private QuoteAmount quoteAmount;               // 원화 금액
+    private ExchangeRate exchangeRate;             // 적용된 환율
     
-    private WithdrawalId withdrawalId;      // 출금 트랜잭션 ID
-    private DepositId depositId;       // 입금 트랜잭션 ID
+    private WithdrawalId withdrawalId;             // 출금 트랜잭션 ID
+    private DepositId depositId;                   // 입금 트랜잭션 ID
     private WithdrawalCancelId withdrawalCancelId; // 출금 취소 트랜잭션 ID
     
     private Finished finished;
@@ -135,83 +134,94 @@ public class ExchangeRequest {
         this.finished = new Finished(true);
     }
 
-    /*
-        환전 관련 이벤트 생성
-     */
+    /* 환전 관련 이벤트 생성 */
+
+    // 환전 시작
     public void exchangeCurrencyStarted() {
         events.add(new ExchangeCurrencyStarted(requestId, exchangeId, direction));
     }
 
+    // 환전 성공
     public void exchangeCurrencySucceeded() {
         events.add(new ExchangeCurrencySucceeded(requestId, exchangeId, direction));
     }
 
+    // 환전 실패
     public void exchangeCurrencyFailed() {
         events.add(new ExchangeCurrencyFailed(requestId, exchangeId, direction));
     }
 
-    /*
-        출금 관련 이벤트 생성
-     */
+    /* 출금 관련 이벤트 생성 */
+
+    // 출금: 성공
     public void withdrawalSucceeded() {
         events.add(new WithdrawalSucceeded(requestId, exchangeId, direction));
     }
 
+    // 출금: 실패
     public void withdrawalFailed() {
         events.add(new WithdrawalFailed(requestId, exchangeId, direction));
     }
 
+    // 출금: 결과 알 수 없음
     public void withdrawalResultUnknown(Count count) {
         events.add(new WithdrawalResultUnknown(requestId, exchangeId, direction, count));
     }
 
-    public void withdrawalSuccessChecked() {
-        events.add(new WithdrawalSuccessChecked(requestId, exchangeId, direction));
-    }
-
-    public void withdrawalFailureChecked() {
-        events.add(new WithdrawalFailureChecked(requestId, exchangeId, direction));
-    }
-
+    // 출금: 취소됨
     public void withdrawalCancelled() {
         events.add(new WithdrawalCancelled(requestId, exchangeId, direction));
     }
 
-    public void delayingWithdrawalCheckRequired(Count count) {
-        events.add(new DelayingWithdrawalCheckRequired(requestId, exchangeId, direction, count));
+    /* 출금 확인 관련 이벤트 처리 */
+
+    // 출금 확인: 성공 확인됨
+    public void withdrawalSuccessChecked() {
+        events.add(new WithdrawalSuccessChecked(requestId, exchangeId, direction));
     }
 
-    /*
-        입금 관련 이벤트 생성
-     */
+    // 출금 확인: 실패 확인됨
+    public void withdrawalFailureChecked() {
+        events.add(new WithdrawalFailureChecked(requestId, exchangeId, direction));
+    }
+
+    // 출금 확인: 결과 알 수 없음
+    public void withdrawalCheckUnknown(Count count) {
+        events.add(new WithdrawalCheckUnknown(requestId, exchangeId, direction, count));
+    }
+
+    /* 입금 관련 이벤트 생성 */
+
+    // 입금: 성공
     public void depositSucceeded() {
         events.add(new DepositSucceeded(requestId, exchangeId, direction));
     }
 
+    // 입금: 실패
     public void depositFailed() {
         events.add(new DepositFailed(requestId, exchangeId, direction));
     }
 
+    // 입금: 결과 알 수 없음
     public void depositResultUnknown(Count count) {
         events.add(new DepositResultUnknown(requestId, exchangeId, direction, count));
     }
 
+    /* 입금 확인 관련 이벤트 처리 */
+
+    // 입금 확인: 성공 확인됨
     public void depositSuccessChecked() {
         events.add(new DepositSuccessChecked(requestId, exchangeId, direction));
     }
 
+    // 입금 확인: 실패 확인됨
     public void depositFailureChecked() {
         events.add(new DepositFailureChecked(requestId, exchangeId, direction));
     }
 
-    public void delayingDepositCheckRequired(Count count) {
-        events.add(new DelayingDepositCheckRequired(requestId, exchangeId, direction, count));
+    // 입금 확인: 결과 알 수 없음
+    public void depositCheckUnknown(Count count) {
+        events.add(new DepositCheckUnknown(requestId, exchangeId, direction, count));
     }
 
-    /*
-        경고 메시지 발송 관련 이벤트 생성
-     */
-    public void sendingWarningMessageRequired(Count count) {
-        events.add(new SendingWarningMessageRequired(requestId, exchangeId, direction, count));
-    }
 }
