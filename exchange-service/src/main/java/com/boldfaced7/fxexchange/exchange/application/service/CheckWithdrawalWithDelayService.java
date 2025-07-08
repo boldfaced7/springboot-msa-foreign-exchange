0709166ef45dbdd886caeaaefff12cf53f2af7f4
boldfaced7 <boldfaced7@naver.com>
2025-07-08 20:18:46 +0900

    Use case 구현체 수정 - 유틸 클래스 제거 - Out port 사용

--- exchange-service/src/main/java/com/boldfaced7/fxexchange/exchange/application/service/CheckWithdrawalWithDelayService.java
+++ exchange-service/src/main/java/com/boldfaced7/fxexchange/exchange/application/service/CheckWithdrawalWithDelayService.java
@@ -3,23 +3,35 @@ package com.boldfaced7.fxexchange.exchange.application.service;
 import com.boldfaced7.fxexchange.common.UseCase;
 import com.boldfaced7.fxexchange.exchange.application.port.in.CheckWithdrawalWithDelayCommand;
 import com.boldfaced7.fxexchange.exchange.application.port.in.CheckWithdrawalWithDelayUseCase;
-import com.boldfaced7.fxexchange.exchange.application.service.util.ExchangeEventPublisher;
+import com.boldfaced7.fxexchange.exchange.application.port.out.cache.LoadExchangeRequestCachePort;
+import com.boldfaced7.fxexchange.exchange.application.port.out.event.PublishEventPort;
+import com.boldfaced7.fxexchange.exchange.domain.exception.ExchangeRequestNotFoundException;
 import com.boldfaced7.fxexchange.exchange.domain.model.ExchangeRequest;
+import com.boldfaced7.fxexchange.exchange.domain.vo.Count;
+import com.boldfaced7.fxexchange.exchange.domain.vo.ExchangeId;
 import lombok.RequiredArgsConstructor;
 
 @UseCase
 @RequiredArgsConstructor
 public class CheckWithdrawalWithDelayService implements CheckWithdrawalWithDelayUseCase {
 
-    private final ExchangeEventPublisher exchangeEventPublisher;
-
+    private final LoadExchangeRequestCachePort loadExchangeRequestCachePort;
+    private final PublishEventPort publishEventPort;
 
     @Override
     public void checkWithdrawalWithDelay(CheckWithdrawalWithDelayCommand command) {
-        exchangeEventPublisher.publishEvents(
-                command.exchangeId(),
-                ExchangeRequest::withdrawalResultUnknown,
-                command.count()
-        );
+        var exchange = getExchange(command.exchangeId());
+        publishExchangeEvent(exchange, command.count());
     }
+
+    private ExchangeRequest getExchange(ExchangeId exchangeId) {
+        return loadExchangeRequestCachePort.loadByExchangeId(exchangeId)
+                .orElseThrow(() -> new ExchangeRequestNotFoundException(exchangeId));
+    }
+
+    private void publishExchangeEvent(ExchangeRequest exchange, Count count) {
+        exchange.markWithdrawalUnknown(count);
+        publishEventPort.publish(exchange);
+    }
+
 }
--- exchange-service/src/main/java/com/boldfaced7/fxexchange/exchange/application/service/CompleteWithdrawalCancelService.java
+++ exchange-service/src/main/java/com/boldfaced7/fxexchange/exchange/application/service/CompleteWithdrawalCancelService.java
@@ -3,9 +3,13 @@ package com.boldfaced7.fxexchange.exchange.application.service;
 import com.boldfaced7.fxexchange.common.UseCase;
 import com.boldfaced7.fxexchange.exchange.application.port.in.CompleteWithdrawalCancelCommand;
 import com.boldfaced7.fxexchange.exchange.application.port.in.CompleteWithdrawalCancelUseCase;
-import com.boldfaced7.fxexchange.exchange.application.service.util.ExchangeEventPublisher;
-import com.boldfaced7.fxexchange.exchange.application.service.util.ExchangeRequestUpdater;
+import com.boldfaced7.fxexchange.exchange.application.port.out.cache.LoadExchangeRequestCachePort;
+import com.boldfaced7.fxexchange.exchange.application.port.out.cancel.SaveWithdrawalCancelPort;
+import com.boldfaced7.fxexchange.exchange.application.port.out.event.PublishEventPort;
+import com.boldfaced7.fxexchange.exchange.domain.exception.ExchangeRequestNotFoundException;
 import com.boldfaced7.fxexchange.exchange.domain.model.ExchangeRequest;
+import com.boldfaced7.fxexchange.exchange.domain.model.WithdrawalCancel;
+import com.boldfaced7.fxexchange.exchange.domain.vo.ExchangeId;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
@@ -13,20 +17,36 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CompleteWithdrawalCancelService implements CompleteWithdrawalCancelUseCase {
 
-    private final ExchangeEventPublisher exchangeEventPublisher;
-    private final ExchangeRequestUpdater exchangeRequestUpdater;
+    private final LoadExchangeRequestCachePort loadExchangeRequestCachePort;
+    private final SaveWithdrawalCancelPort saveWithdrawalCancelPort;
+    private final PublishEventPort publishEventPort;
 
     @Override
     @Transactional
     public void completeWithdrawalCancel(CompleteWithdrawalCancelCommand command) {
-        exchangeRequestUpdater.update(
+        var exchange = getExchange(command.exchangeId());
+        var withdrawalCancel = toDomain(exchange, command);
+
+        publishEventPort.publish(withdrawalCancel);
+        saveWithdrawalCancelPort.saveWithdrawalCancel(withdrawalCancel);
+    }
+
+    private ExchangeRequest getExchange(ExchangeId exchangeId) {
+        return loadExchangeRequestCachePort.loadByExchangeId(exchangeId)
+                .orElseThrow(() -> new ExchangeRequestNotFoundException(exchangeId));
+    }
+
+    private WithdrawalCancel toDomain(
+            ExchangeRequest exchange,
+            CompleteWithdrawalCancelCommand command
+    ) {
+        return WithdrawalCancel.create(
+                command.withdrawalCancelId(),
+                exchange.getRequestId(),
                 command.exchangeId(),
-                ExchangeRequest::addWithdrawalCancelId,
-                command.withdrawalCancelId()
-        );
-        exchangeEventPublisher.publishEvents(
-                command.exchangeId(),
-                ExchangeRequest::withdrawalCancelled
+                exchange.getUserId(),
+                command.direction(),
+                true
         );
     }
 }
