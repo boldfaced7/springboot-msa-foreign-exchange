2c47b4d5da10fd537aa53a8b88f6761334f71b60
boldfaced7 <boldfaced7@naver.com>
2025-07-08 20:10:29 +0900

    이벤트 발행 포트 수정: 도메인 객체를 파라미터로 받도록 수정

--- exchange-service/src/main/java/com/boldfaced7/fxexchange/exchange/application/port/out/PublishExchangeEventPort.java
+++
@@ -1,9 +0,0 @@
-package com.boldfaced7.fxexchange.exchange.application.port.out;
-
-import com.boldfaced7.fxexchange.exchange.domain.event.DomainEvent;
-
-import java.util.List;
-
-public interface PublishExchangeEventPort {
-    void publish(List<DomainEvent> events);
-}
